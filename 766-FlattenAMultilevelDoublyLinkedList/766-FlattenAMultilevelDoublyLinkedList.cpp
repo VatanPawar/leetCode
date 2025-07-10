af76880bf36f3bd6a02469a15546266757e62168
VatanPawar <221239@iiitt.ac.in>
2025-07-11 00:07:08 +0530

    Create 766-FlattenAMultilevelDoublyLinkedList

---
+++ 766-FlattenAMultilevelDoublyLinkedList/766-FlattenAMultilevelDoublyLinkedList.cpp
@@ -0,0 +1,43 @@
+// Last updated: 7/11/2025, 12:07:04 AM
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node*solve(Node* head){
+        if(!head){
+            return head;
+        }
+        auto it=head;
+        auto tail=it;
+        while(it){
+            if(it->child){
+            auto childTail=solve(it->child);
+
+            auto temp=it->next;
+            it->next=it->child;
+            it->next->prev=it;
+            childTail->next=temp;
+            if(temp){
+                temp->prev=childTail;
+            }
+            it->child=NULL;
+        }
+        
+        tail=it;
+        it=it->next;}
+        return tail;
+    }
+    Node* flatten(Node* head) {
+        solve(head);
+        return head;   
+    }
+};
