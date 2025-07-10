e12704ad383a46ce3404673a6f3429b4f6ae9455
VatanPawar <221239@iiitt.ac.in>
2025-07-11 00:06:07 +0530

    Create 1050-ConstructBinarySearchTreeFromPreorderTraversal

---
+++ 1050-ConstructBinarySearchTreeFromPreorderTraversal/1050-ConstructBinarySearchTreeFromPreorderTraversal.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/11/2025, 12:06:03 AM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i=0;
+        return build( i,preorder,INT_MAX);
+    }
+    TreeNode*build(int& i,vector<int>& preorder,int bound)
+    {
+       if(i == preorder.size() || preorder[i] > bound) return nullptr;
+       TreeNode* root = new TreeNode(preorder[i++]);
+       root->left=build( i,preorder,root->val);
+       root->right=build( i,preorder,bound);
+       return root;
+    }
+};
