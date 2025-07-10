80eb8bcbf0d57b28eaf1d6711d2fbaee957c4642
VatanPawar <221239@iiitt.ac.in>
2025-07-11 00:05:05 +0530

    Create 1508-LongestHappyPrefix

---
+++ 1508-LongestHappyPrefix/1508-LongestHappyPrefix.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/11/2025, 12:05:01 AM
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int n = s.size();
+        vector<int> lps(n, 0);
+        int len = 0;
+        for(int i = 1; i < n; i++) {
+            while(len > 0 && s[i] != s[len]) {
+                len = lps[len - 1];
+            }
+            if(s[i] == s[len]) {
+                len++;
+                lps[i] = len;
+            }
+        }
+        return s.substr(0, lps[n - 1]);
+    }
+};
