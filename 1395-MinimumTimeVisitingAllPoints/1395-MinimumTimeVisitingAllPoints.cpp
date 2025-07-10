054113b7b1a7c6a06755ad79aea958061b70075c
VatanPawar <221239@iiitt.ac.in>
2025-07-11 00:05:26 +0530

    Create 1395-MinimumTimeVisitingAllPoints

---
+++ 1395-MinimumTimeVisitingAllPoints/1395-MinimumTimeVisitingAllPoints.cpp
@@ -0,0 +1,13 @@
+// Last updated: 7/11/2025, 12:05:22 AM
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int time = 0;
+        for (int i = 1; i < points.size(); ++i) {
+            int dx = abs(points[i][0] - points[i - 1][0]);
+            int dy = abs(points[i][1] - points[i - 1][1]);
+            time += max(dx, dy);
+        }
+        return time;
+    }
+};
